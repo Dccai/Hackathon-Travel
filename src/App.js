489ebc968f50bb98c33231917f7e16b8a97c4ac6
Dccai <deanc2718@gmail.com>
2023-06-24 07:20:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { MainPage } from './Pages/MainPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and sd.
-        </p>
-        
-      </header>
+     <MainPage/>
     </div>
   );
 }
