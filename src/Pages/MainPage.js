c2604b5fba38ff62c3961fbc248090a5f5fde42e
Dccai <deanc2718@gmail.com>
2023-06-24 07:59:33 +0800

    Update MainPage.js

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -5,15 +5,19 @@ export class MainPage extends React.Component{
     constructor(props){
         super(props);
         this.handleMenuClick=this.handleMenuClick.bind(this);
+        this.state={menuVisibility:true};
     }
     handleMenuClick(){
-       return; 
+       if(this.state.menuVisibility){this.setState({menuVisibility:false})}
+       else{
+        this.setState({menuVisibility:true});
+       }
     }
     render(){
         return (
             <>
-        <h1>Travel Icon Options</h1>
-        <div onClick={this.handleMenuClick}>Menu</div>
+        <h1 onClick={this.handleMenuClick}>Travel Icon Options</h1>
+        <div style={{invisible:this.state.menuVisibility}} >Menu</div>
 <Draggable><h1>Hi</h1></Draggable>
 </>
         );
