24827418fe9d28bffcae93104aeeda9faeb750df
Dccai <deanc2718@gmail.com>
2023-06-24 07:12:04 +0800

    fgtegt
    dserghehge

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import React from "react";
 export class MainPage extends React.Component{
     constructor(props){
         super(props);
     }
-   
-}
-=======
+    render(){
+        return (
+<h1></h1>
+        );
 
->>>>>>> c855e5aa34a1bbeb8863c63e892e670ec82984a2
-=======
+    }
 
->>>>>>> c855e5aa34a1bbeb8863c63e892e670ec82984a2
+   }
