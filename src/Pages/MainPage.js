8e91c92f66cb642c05a8adf1b23230519cb1ad44
Dccai <deanc2718@gmail.com>
2023-06-24 07:53:00 +0800

    Update MainPage.js

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -4,10 +4,18 @@ import Draggable,{DraggableCore} from "react-draggable";
 export class MainPage extends React.Component{
     constructor(props){
         super(props);
+        this.handleMenuClick=this.bind(handleMenuClick);
+    }
+    handleMenuClick(){
+       return; 
     }
     render(){
         return (
+            <>
+        <h1>Travel Icon Options</h1>
+        <div onClick={this.handleMenuClick}>Menu</div>
 <Draggable><h1>Hi</h1></Draggable>
+</>
         );
 
     }
