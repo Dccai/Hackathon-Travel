0a73d8e5de7c4deb5d679307b5eeaaea3fde4f21
Dccai <deanc2718@gmail.com>
2023-06-24 10:43:48 +0800

    Update AddBlock.js

--- src/Components/AddBlock.js
+++ src/Components/AddBlock.js
@@ -1,1 +1,7 @@
-
+import React from "react";
+import Draggable,{DraggableCore} from "react-draggable";
+export function AddBlock (props){
+if(props.type==="red"){
+    return <Draggable><input>Hello</input></Draggable>
+}
+}
