a0d7c85995779875509232863bcf4b017911b976
Dccai <deanc2718@gmail.com>
2023-06-24 11:06:02 +0800

    add

--- src/Components/AddBlock.js
+++ src/Components/AddBlock.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
+import { ReactDOM } from "react";
 import Draggable,{DraggableCore} from "react-draggable";
 export function AddBlock (props){
-
-return (<><Draggable><p>Why Name Is John Cena</p></Draggable><p>Hello I am Dean</p></>);
+let [blocks,addBlocks]=useState([]);
+if(props.type==="red"){
+    let newBlock=React.createElement("div",{style:{color:"red"}},<><p>Hello tHis is a red block</p></>)
+    addBlocks(oldArray=>[...oldArray,newBlock]);
 }
+else{ let newBlock=React.createElement("div",{style:{color:"blue"}},<><p>Hello tHis is a blue block</p></>)
+addBlocks(oldArray=>[...oldArray,newBlock]);}
+return (<>{blocks.map(a=><Draggable>{a}</Draggable>)}</>);
+}
+ReactDOM.render(<AddBlock/>,document.getElementById("blocks"));
