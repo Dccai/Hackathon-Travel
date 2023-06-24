e3d0984dae2f2601b96cbce2dacb45ba0007037c
Dccai <deanc2718@gmail.com>
2023-06-24 12:42:59 +0800

    Update MainPage.js

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import Draggable,{DraggableCore} from "react-draggable";
+import "./MainPage.css";
 export class MainPage extends React.Component{
     constructor(props){
         super(props);
         this.handleMenuClick=this.handleMenuClick.bind(this);
         this.handleTravelClick=this.handleTravelClick.bind(this);
+        this.handlePhotoClick=this.handlePhotoClick.bind(this);
+        this.handleDestinationBlock=this.handleDestinationBlock.bind(this);
         this.state={menuVisibility:0,travelBlocksToAdd:["travel","travel"]};
         this.placeholderBlock=undefined;
     }
@@ -20,17 +23,27 @@ export class MainPage extends React.Component{
         this.placeholderBlock=this.state.travelBlocksToAdd;
         this.placeholderBlock.push("travel");
         this.setState({travelBlocksToAdd:this.placeholderBlock});
-        alert(this.state.travelBlocksToAdd);
+    }
+    handlePhotoClick(){
+        this.placeholderBlock=this.state.travelBlocksToAdd;
+        this.placeholderBlock.push("photo");
+        this.setState({travelBlocksToAdd:this.placeholderBlock});
+    }
+    handleDestinationBlock(){
+        this.placeholderBlock=this.state.travelBlocksToAdd;
+        this.placeholderBlock.push("destination");
+        this.setState({travelBlocksToAdd:this.placeholderBlock});
     }
     render(){
         return (
             <>
         <h1 onClick={this.handleMenuClick}>Travel Icon Options</h1>
-        <div id="Menu" style={{color:"blue"}} ><button style={{color:"red"}} onClick={this.handleTravelClick}>Travel Block</button></div>
+        <div id="Menu" style={{color:"blue"}} ><button style={{color:"red"}} onClick={this.handleTravelClick}>Travel Block</button><button onClick={this.handlePhotoClick}>Picture Block</button><button onClick={this.handleDestinationBlock}>Destination Block</button></div>
 <Draggable><h1>Hi</h1></Draggable>
 <ul>{this.state.travelBlocksToAdd.map((a,h)=>{
-    if(a==="travel"){return <li key={h}><Draggable><h1>Travel</h1></Draggable></li>;}
-    else{return <li key={h}><Draggable><h1>Other</h1></Draggable></li>;}
+    if(a==="travel"){return <Draggable><li key={h}><h1>Travel</h1></li></Draggable>;}
+    else if(a==="photo"){return <Draggable><li key={h}><h1>Image</h1></li></Draggable>;}
+    else{return <Draggable><li key={h}><h1>Destination</h1></li></Draggable>;}
 })}</ul>
 </>
         );
