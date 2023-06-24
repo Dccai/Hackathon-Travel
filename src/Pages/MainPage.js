190f14ca372cc570199167348c89d789e5abf062
Dccai <deanc2718@gmail.com>
2023-06-24 23:52:54 +0800

    sdfsf

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -10,6 +10,7 @@ export class MainPage extends React.Component{
         this.handlePhotoClick=this.handlePhotoClick.bind(this);
         this.handleDestinationBlock=this.handleDestinationBlock.bind(this);
         this.handleTravelSubmit=this.handleTravelSubmit.bind(this);
+        this.handlePhotoSubmit=this.handlePhotoSubmit.bind(this);
         this.state={menuVisibility:0,travelBlocksToAdd:["travel","travel"],menuItemLoc:0};
         this.placeholderBlock=undefined;
         this.formData=undefined;
@@ -26,6 +27,27 @@ export class MainPage extends React.Component{
             var b=document.createElement('li');
             b.innerHTML=`${key}: ${this.formEntries[key]}`;
             parent.appendChild(b);
+            }
+        }
+    }
+    handlePhotoSubmit(event){
+        event.preventDefault();
+        this.formPlaceHolder=new FormData(event.currentTarget);
+        this.formEntries=Object.fromEntries(this.formPlaceHolder);
+        var parent=event.currentTarget.parentNode;
+        event.currentTarget.remove();
+        for (var key in this.formEntries){
+            if(this.formEntries[key].length!=0){
+            if(key==="Landmark-Image"){
+                var b=document.createElement('img');
+                b.src=this.formEntries[key];
+                parent.appendChild(b);
+            }
+            else{
+            var b=document.createElement('li');
+            b.innerHTML=`${key}: ${this.formEntries[key]}`;
+            parent.appendChild(b);
+            }
             }
         }
     }
@@ -59,8 +81,8 @@ export class MainPage extends React.Component{
 <ul>{this.state.travelBlocksToAdd.map((a,h)=>{
     if(a==="travel"){return (<Draggable><li className="travelBlock"key={h}><h1>Travel</h1><form onSubmit={this.handleTravelSubmit}><label htmlFor="Transport">Transportation</label>
     <input name="Transport"type="text"></input><label htmlFor="Image">Photo</label><input type="text" name="Image"></input>
-    <label htmlFor="Date and Time">Time and Date</label><input type="text" name="Date and Time"></input><div><input type="text" name="From"></input> to <input name="To" type="text"></input></div><input type="submit" ></input></form></li></Draggable>);}
-    else if(a==="photo"){return <Draggable><li className="photoBlock"key={h}><h1>Image</h1></li></Draggable>;}
+    <label htmlFor="Date and Time">Time and Date</label><input type="text" name="Date and Time"></input><div><input type="text" name="From"></input> to <input name="To" type="text"></input></div><label htmlFor="Travel Reminders">Reminders</label><input type="text" name="Travel Reminders"></input><label htmlFor="Travel Seats">Seats</label><input name="Travel Seats"></input><input type="submit" ></input></form></li></Draggable>);}
+    else if(a==="photo"){return (<Draggable><li className="photoBlock"key={h}><h1>Image</h1><form onSubmit={this.handlePhotoSubmit}><label htmlFor="Landmark-Image"></label><input name="Landmark-Image" type="text"></input></form></li></Draggable>);}
     else{return <Draggable><li className="destBlock"key={h}><h1>Destination</h1></li></Draggable>;}
 })}</ul>
 </>
