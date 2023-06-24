3544a28683609f2e8084a5f93b677d217973ff67
Dccai <deanc2718@gmail.com>
2023-06-24 11:19:46 +0800

    Update AddBlock.js

--- src/Components/AddBlock.js
+++ src/Components/AddBlock.js
@@ -8,5 +8,5 @@ if(props.type==="red"){
 }
 else{ let newBlock=React.createElement("div",{style:{color:"blue"}},<><p>Hello tHis is a blue block</p></>)
 addBlocks(oldArray=>[...oldArray,newBlock]);}
-return (<><ul>{blocks.map(a=><Draggable>{<li>a</li>}</Draggable>)}</ul></>);
+return (<><ul>{blocks.map(a=><Draggable><li>{a}</li></Draggable>)}</ul></>);
 }
