e115d7e0d2158a9c5e8e02fbbcd9a2a9998fa63b
Dccai <deanc2718@gmail.com>
2023-06-24 12:16:00 +0800

    Update MainPage.js

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,13 +1,13 @@
 
 import React from "react";
 import Draggable,{DraggableCore} from "react-draggable";
-import { AddBlock } from "../Components/AddBlock";
 export class MainPage extends React.Component{
     constructor(props){
         super(props);
         this.handleMenuClick=this.handleMenuClick.bind(this);
         this.handleTravelClick=this.handleTravelClick.bind(this);
-        this.state={menuVisibility:0,blocksToAdd:[]};
+        this.state={menuVisibility:0,travelBlocksToAdd:[]};
+        this.placeholderBlock=undefined;
     }
     handleMenuClick(){
        if(this.state.menuVisibility===0){this.setState({menuVisibility:100})}
@@ -17,7 +17,8 @@ export class MainPage extends React.Component{
        document.getElementById("Menu").style.opacity=this.state.menuVisibility;
     }
     handleTravelClick(){
-       return;
+        this.placeholderBlock=React.createElement("div",{style:{color:"red"}},<p></p>);
+        this.state.travelBlocksToAdd.push(this.placeholderBlock);
     }
     render(){
         return (
@@ -25,8 +26,7 @@ export class MainPage extends React.Component{
         <h1 onClick={this.handleMenuClick}>Travel Icon Options</h1>
         <div id="Menu" style={{color:"blue"}} ><button style={{color:"red"}} onClick={this.handleTravelClick}>Travel Block</button></div>
 <Draggable><h1>Hi</h1></Draggable>
-<AddBlock type="red"/>
-
+<ul>{this.state.travelBlocksToAdd.map((a,h)=><li key={h}></li>)}</ul>
 </>
         );
 
