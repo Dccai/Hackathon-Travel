42cfe998f184f06db24cc1a4fa33c0300062eca6
Dccai <deanc2718@gmail.com>
2023-06-24 11:07:25 +0800

    Update AddBlock.js

--- src/Components/AddBlock.js
+++ src/Components/AddBlock.js
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import { ReactDOM } from "react-dom";
+import { ReactDOM } from "react-dom/client";
 import Draggable,{DraggableCore} from "react-draggable";
 export function AddBlock (props){
 let [blocks,addBlocks]=useState([]);
