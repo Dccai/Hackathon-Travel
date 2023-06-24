c1415ce85584cb70caa8a0e0fdf09a8f33272650
Dccai <deanc2718@gmail.com>
2023-06-24 10:46:01 +0800

    Update AddBlock.js

--- src/Components/AddBlock.js
+++ src/Components/AddBlock.js
@@ -2,7 +2,7 @@ import React from "react";
 import Draggable,{DraggableCore} from "react-draggable";
 export function AddBlock (props){
 if(props.type==="red"){
-    return <Draggable><input>Hello</input></Draggable>
+    return <Draggable><input>Hello</input></Draggable>;
 }
-return <Draggable><p>Why Name Is John Cena</p></Draggable>
+return <Draggable><p>Why Name Is John Cena</p></Draggable>;
 }
