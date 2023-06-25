10e53d6c4c262ffd5540c062f18d652ed05c5848
Dccai <deanc2718@gmail.com>
2023-06-25 08:11:26 +0800

    Update MainPage.js

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Draggable,{DraggableCore} from "react-draggable";
+import Xarrow,{Xwrapper} from "react-xarrows";
 import "./MainPage.css";
 export class MainPage extends React.Component{
     constructor(props){
@@ -12,11 +13,50 @@ export class MainPage extends React.Component{
         this.handleTravelSubmit=this.handleTravelSubmit.bind(this);
         this.handlePhotoSubmit=this.handlePhotoSubmit.bind(this);
         this.handleSightSeeingSubmit=this.handleSightSeeingSubmit.bind(this);
+        this.nodeConnector=this.nodeConnector.bind(this);
+        this.nodeSetter=this.nodeSetter.bind(this);
         this.state={menuVisibility:0,travelBlocksToAdd:["travel","travel"],menuItemLoc:0};
         this.placeholderBlock=undefined;
         this.formData=undefined;
         this.formEntries=undefined;
+        this.nodeBearer=React.createRef();
+        this.nodes=React.createRef();
+        this.canSetNode=React.createRef();
+        this.nodeArray=[]
+        this.spawnConnections=this.spawnConnections.bind(this);
     }
+    componentDidMount(){
+this.nodeBearer.current=new Map();
+this.nodes.current=[];
+this.canSetNode.current=true;
+    }
+    spawnConnections(){
+        if(this.nodeBearer.current!==null){
+        for(var a of this.nodeBearer.current.keys()){
+            console.log(this.nodeBearer.current);
+            this.nodeBearer.current.get(a).neighbours.forEach(b=>{this.nodeArray.push([a,b]);});
+        }
+        }
+    }
+   nodeSetter(id){
+    for(var c of this.nodeBearer.current.keys()){
+        if(id.toString()===c){return;}
+    }
+   this.nodeBearer.current.set(id.toString(),{neighbours:[]});
+   }
+   nodeConnector(event){
+    if(this.canSetNode.current){
+        this.canSetNode.current=false;
+        this.nodes.current.push(event.target.id.toString());
+    }
+    else{
+        
+        this.nodeBearer.current.get(this.nodes.current[0]).neighbours.push(event.target.id.toString());
+        //this.nodeBearer.current.get(this.nodes.current[0].toString()).push(event.target.id);
+        this.canSetNode.current=true;
+        this.nodes.current.pop();
+    }
+   }
     handleTravelSubmit(event){
         event.preventDefault();
         this.formPlaceHolder=new FormData(event.currentTarget);
@@ -124,9 +164,11 @@ export class MainPage extends React.Component{
     <input name="Transport"type="text"></input><label htmlFor="Image">Photo</label><input type="text" name="Image"></input>
     <label htmlFor="Date and Time">Time and Date</label><input type="text" name="Date and Time"></input><div><input type="text" name="From"></input> to <input name="To" type="text"></input></div><label htmlFor="Travel Reminders">Reminders</label><input type="text" name="Travel Reminders"></input><label htmlFor="Travel Seats">Seats</label><input name="Travel Seats"></input><input type="submit" ></input></form></li></Draggable>);}
     else if(a==="photo"){return (<Draggable><li className="photoBlock"key={h}><h1>Image</h1><form onSubmit={this.handlePhotoSubmit}><label htmlFor="Pictures"></label><input name="Pictures" type="text"></input></form></li></Draggable>);}
-    else{return (<Draggable><li className="destBlock"key={h}><div style={{borderRadius:"50%",backgroundColor:"black",width:"50px",height:'50px',marginLeft:"auto",marginRight:"auto",position:'relative',top:"-20px"}}>  </div><h1>Destination</h1><form onSubmit={this.handleSightSeeingSubmit}><label htmlFor="SightSeeing Site">Site Photo</label><input name="SightSeeing Site" type="text"></input><label htmlFor="Tickets and Prices">Tickets and Prices</label><input name="Tickets and Prices" type="text"></input><input type="submit"></input></form></li></Draggable>);}
+    else{
+        this.nodeSetter(h);
+        return (<Draggable><li className="destBlock"key={h}><div id={h} onClick={this.nodeConnector} onDrag={this.arrowUpdater} onStop={this.arrowUpdater} style={{borderRadius:"50%",backgroundColor:"black",width:"50px",height:'50px',marginLeft:"auto",marginRight:"auto",position:'relative',top:"-20px"}}>  </div><h1>Destination</h1><form onSubmit={this.handleSightSeeingSubmit}><label htmlFor="SightSeeing Site">Site Photo</label><input name="SightSeeing Site" type="text"></input><label htmlFor="Tickets and Prices">Tickets and Prices</label><input name="Tickets and Prices" type="text"></input><input type="submit"></input></form></li></Draggable>);}
 })}</ul>
-<svg width="300" height="300"><line x1="30" y1="30" x2="90" y2="60" stroke="black"/></svg>
+ {this.spawnConnections()}{this.nodeArray.map(z=>{return <Xwrapper><Xarrow start={z[0]} end={z[1]}/></Xwrapper>;})}
 </>
         );
 
